ee86c822ae2debb0359f232a1f77d2a76be83630
夜影、NightShadow <169712528+1224HuangJin@users.noreply.github.com>
2026-04-20 15:44:33 +0200

    添加 index.js 来让 Layout.vue 可运行

---
+++ .vitepress/theme/index.js
@@ -0,0 +1,8 @@
+import DefaultTheme from 'vitepress/theme'
+import MyLayout from '/Layout.vue' // 导入你刚创建的 Layout.vue
+
+export default {
+  extends: DefaultTheme,
+  // 使用自定义的 Layout 组件
+  Layout: MyLayout
+}
